7e801813404307f88f05c203c702987ab53fffc5
Junho Park <nari40@kyonggi.ac.kr>
2026-02-05 07:36:04 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C99/백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.c
+++ C99/백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.c
@@ -2,31 +2,17 @@
 
 int main() {
     int x, y, w, h;
-
     scanf("%d %d %d %d", &x, &y, &w, &h);
 
-    int min_x;
-    if (x < w - x) {
-        min_x = x;
-    }
-    else {
-        min_x = w - x;
+    int arr[4] = {x, w - x, y, h - y};
+    int min = arr[0];
+
+    for (int i = 1; i < 4; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
     }
 
-    int min_y;
-    if (y < h - y) {
-        min_y = y;
-    }
-    else {
-        min_y = h - y;
-    }
-
-    if (min_x < min_y) {
-        printf("%d", min_x);
-    }
-    else {
-        printf("%d", min_y);
-    }
-
+    printf("%d", min);
     return 0;
 }
