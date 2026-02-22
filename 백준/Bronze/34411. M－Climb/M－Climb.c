9fc47f2237de713b1fe20ff63143e7f68cb0147e
Junho Park <nari40@kyonggi.ac.kr>
2026-02-22 22:05:57 +0900

    [Bronze V] Title: M-Climb, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int m, n, y;
+    
+    scanf("%d", &m);
+    scanf("%d", &n);
+    scanf("%d", &y);
+    
+    printf("%d\n", m * n);
+    
+    return 0;
+}
